a0eadb237e15f40016c209477b6b17336f2c9e01
Tianyu Liu <terryliu@berkeley.edu>
2022-10-25 18:08:53 -0700

    Added first page Button & Google signin

--- web/src/components/User/Login/Login.tsx
+++ web/src/components/User/Login/Login.tsx
@@ -1,11 +1,49 @@
-import React, { useState } from "react";
-// import "./Login.css";
+import { Button } from "antd";
+import "./Login.css";
+import ReactDOM from "react-dom";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { createRoot } from "react-dom/client";
+import { GoogleLogin } from '@react-oauth/google';
+import React from 'react';
+
+
+const handleEmailAuth = () => {
+  console.log('EmailAuth Clicked');
+}
+
+const handleGoogleAuth = () => {
+  console.log('GoogleAuth Clicked');
+  <div>
+    <h1>Hellow</h1>
+  </div>
+          
+}
 
 const Login = () => {
   console.log("Login");
   return (
-    <div>
-      <h1>FIXMMMM</h1>
+    <div id="main-container">
+      <div id="login-wrapper" className="hidden">
+        <h1>Cal Course</h1>
+      <div>我们需要验证你的学生身份</div>
+      <Button onClick={handleEmailAuth}>邮箱验证码</Button> <Button onClick={handleGoogleAuth}>bConnected</Button>
+
+      <GoogleOAuthProvider clientId=
+      "250149314571-jen9j3rq3bsds17t8ot35g4efd66gt54.apps.googleusercontent.com">
+        <GoogleLogin
+  onSuccess={credentialResponse => {
+    console.log(credentialResponse);
+  }}
+  onError={() => {
+    console.log('Login Failed');
+  }}
+  useOneTap
+/></GoogleOAuthProvider></div>
+<div id="footer">
+  <Button shape="round">About</Button>
+  <Button shape="round">🍪</Button>
+  <Button shape="round">？</Button>
+</div>
     </div>
   );
 };
