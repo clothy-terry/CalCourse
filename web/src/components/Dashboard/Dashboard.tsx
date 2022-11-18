e4cb1873f575e450b04d664d4442b74d09e4f8b1
Tianyu Liu <terryliu@berkeley.edu>
2022-11-17 16:39:04 -0800

    Merge branch 'Login' of https://github.com/CalCourse-Dev/CalCourse_FrontEnd into Login

--- web/src/components/Dashboard/Dashboard.tsx
+++ web/src/components/Dashboard/Dashboard.tsx
@@ -1,7 +1,10 @@
 import { createContext } from "react";
 import "./Dashboard.css";
+
+
 const courseContext = createContext({});
 const Dashboard = () => {
+
   return (
     <div>
       <h1>FIXME</h1>
