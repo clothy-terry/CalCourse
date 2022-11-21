2baec8db04ed089fc1eb9ab8ae11e31e7762643a
YiweiIvy <105534550+YiweiIvy@users.noreply.github.com>
2022-11-20 20:54:18 -0800

    basic panel card layout

--- web/src/components/AcademicPanel/AcademicPanel.tsx
+++ web/src/components/AcademicPanel/AcademicPanel.tsx
@@ -1,11 +1,28 @@
 import React, { useState } from "react";
 import PanelCard from "./PanelCard/PanelCard";
+import "./AcademicPanel.css"
 
-const AcademicPanel = () => {
+const AcademicPanel = (props:any) => {
   return (
-    <div>
-      <h1>FIXME</h1>
+    <div id="panel-container">
+      <div className="panel">
+        <div className="id-wrapper">
+          <div className="id">
+            <span>
+              <span>{props["course_name"]}</span>
+              <div>CS61A</div>
+            </span>
+          </div>
+        </div>
+        <div className="panel_card">
+          <PanelCard name = 'Website' link = '123'/>
+          <PanelCard name = 'Gradescope' link = '456'/>
+          <PanelCard name = 'Ed' link = '789'/>
+          </div>
+        <div className="desc">{props["course_id"]}</div>
+      </div>
     </div>
   );
 };
+
 export default AcademicPanel;
--- web/src/components/AcademicPanel/PanelCard/PanelCard.tsx
+++ web/src/components/AcademicPanel/PanelCard/PanelCard.tsx
@@ -1,10 +1,40 @@
 import React, { useState } from "react";
+import "./PanelCard.css"
 
-const PanelCard = () => {
+import { Button, DatePicker, Space, version } from "antd";
+import 'antd/dist/reset.css';
+
+const PanelCard = (props: { name: String; link: String }) => {
   return (
-    <div>
-      <h1>FIXME</h1>
+    <div id="card-container">
+      <Space>
+        <div className="card1">
+        <div className="part">
+          {/* <Button type="text" block>
+            Website
+          </Button> */}
+            <div>{props.name}</div>
+        </div>
+      </div>
+      </Space>
     </div>
   );
 };
+
+// const PanelCard = (props:any) => {
+//   return (
+//     <div id="card-container">
+//       <Space>
+//         <div className="card1">
+//         <div className="part">
+//           {/* <Button type="text" block>
+//             Website
+//           </Button> */}
+//             <div>Website</div>
+//         </div>
+//       </div>
+//       </Space>
+//     </div>
+//   );
+// };
 export default PanelCard;
