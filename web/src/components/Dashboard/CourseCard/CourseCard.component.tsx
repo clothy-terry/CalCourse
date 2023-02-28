481b0de5287033a962e9d34d3085ff9a42966f93
akmazian <azz@berkeley.edu>
2023-02-27 23:19:36 -0800

    progress

--- web/src/components/Dashboard/CourseCard/CourseCard.component.tsx
+++ web/src/components/Dashboard/CourseCard/CourseCard.component.tsx
@@ -1,45 +1,47 @@
 import { Transition } from '@headlessui/react'
 import { QRCodeSVG } from 'qrcode.react'
-import { useState } from 'react'
+import { Component } from 'react'
 import type { CourseData } from '../../../utils/interfaces'
 
-const CourseCard = (course: CourseData) => {
-    const { course_name, course_id, course_qr_code_url } = course
-    var a = false
+interface ICourseCardProps {
+    course: CourseData
+}
 
-    // const [showing_qr_code, set_showing_qr_code] = useState(false)
+interface ICourseCardStates {
+    showing_details: boolean
+}
 
-    return (
-        <div
-            className="card-transluscent"
-            onClick={() => {
-                a = !a
-                console.log(a)
-            }}
-        >
-            <h1 className="text-subtitle">{course_name}</h1>
-            <label>{course_id}</label>
-            <div>
-                <Transition
-                    show={a ? true : false}
-                    enter="transform transition duration-[400ms]"
-                    enterFrom="opacity-0 rotate-[-120deg] scale-50"
-                    enterTo="opacity-100 rotate-0 scale-100"
-                    leave="transform duration-200 transition ease-in-out"
-                    leaveFrom="opacity-100 rotate-0 scale-100"
-                    leaveTo="opacity-0 scale-95"
-                >
+class CourseCard extends Component<ICourseCardProps, ICourseCardStates> {
+    state: ICourseCardStates = {
+        showing_details: false,
+    }
+
+    render() {
+        return (
+            <div
+                className={`card-transluscent h-64 ${this.state.showing_details ? 'col-span-2' : ''} duration-150`}
+                onClick={() => {
+                    this.setState(state => ({
+                        showing_details: !state.showing_details,
+                    }))
+                }}
+            >
+                <h1 className="text-subtitle">
+                    {this.props.course.course_name}
+                </h1>
+                <label>{this.props.course.course_id}</label>
+                <div className={`${this.state.showing_details ? '' : 'hidden'} duration-200`}>
                     <QRCodeSVG
                         className="img"
-                        value={course_qr_code_url}
+                        value={this.props.course.course_qr_code_url}
                         size={280}
                         bgColor="transparent"
                         fgColor="#333"
                     />
-                </Transition>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default CourseCard
